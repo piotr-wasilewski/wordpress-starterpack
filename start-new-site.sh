6ac44b572a37edabaa776e3556de02a26f08c9ac
User Bfcior <bfcior@s37.mydevil.net>
2019-04-05 16:18:36 +0200

    bash script

--- start-new-site.sh
+++ start-new-site.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+if [ -z "$ENV_MYSQL_HOST" ]
+then
+      echo "Brak wpisu w .profile"
+      echo "export \$ENV_MYSQL_HOST=host"
+      exit 1
+fi
+
+if [ -z "$ENV_MYSQL_USER" ]
+then
+      echo "Brak wpisu w .profile"
+      echo "export \$ENV_MYSQL_USER=user"
+      exit 1
+fi
+
+if [ -z "$ENV_MYSQL_PASS" ]
+then
+      echo "Brak wpisu w .profile"
+      echo "export \$ENV_MYSQL_PASS=password"
+      exit 1
+fi
+
 DIR=~/domains/projekty.pwa.com.pl/public_html/
 WP=wordpress-starterpack
 SITE=`echo "print('$1'.lower())" | python` # to lower case
 DNS=http://projekty.pwa.com.pl/$SITE
-MYSQL_USER=m1547_projects
-MYSQL_SITE_DB=m1547_$SITE
+MYSQL_SITE_DB="${ENV_MYSQL_USER}_${SITE}"
 
 if [ -z "$SITE" ]; then
   echo "Nie podano domeny. Proszę podać jako parametr nową domenę"
@@ -24,14 +44,15 @@ fi
 
 echo "Rozpoczynam kopiowanie danych"
 cp -r $DIR$WP $DIR$SITE
+rm $DIR$SITE/start-new-site.sh
 echo "Folder \"$SITE\" utworzony poprawnie"
 
 echo "Rozpoczynam kopiowanie bazy danych MySQL"
-devil mysql db add $SITE $MYSQL_USER
+devil mysql db add projects_${SITE} $ENV_MYSQL_USER
 
-mysqldump --host mysql11.mydevil.net --user $MYSQL_USER -pnQpw2ysSqrwBakXRG0vR m1547_wp_starterpack > $MYSQL_SITE_DB.sql
-mysql --host mysql11.mydevil.net --user $MYSQL_USER -pnQpw2ysSqrwBakXRG0vR $MYSQL_SITE_DB < $MYSQL_SITE_DB.sql
-mysql --host mysql11.mydevil.net --user $MYSQL_USER -pnQpw2ysSqrwBakXRG0vR $MYSQL_SITE_DB -e "UPDATE wp_options SET option_value = '$DNS' WHERE wp_options.option_id IN (1,2);"
+mysqldump --host $ENV_MYSQL_HOST --user $ENV_MYSQL_USER -p$ENV_MYSQL_PASS ${ENV_MYSQL_USER}_wp_starterpack > $MYSQL_SITE_DB.sql
+mysql --host $ENV_MYSQL_HOST --user $ENV_MYSQL_USER -p$ENV_MYSQL_PASS $MYSQL_SITE_DB < $MYSQL_SITE_DB.sql
+mysql --host $ENV_MYSQL_HOST --user $ENV_MYSQL_USER -p$ENV_MYSQL_PASS $MYSQL_SITE_DB -e "UPDATE wp_options SET option_value = '$DNS' WHERE wp_options.option_id IN (1,2);"
 
 rm $MYSQL_SITE_DB.sql
 
@@ -45,11 +66,21 @@ RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule . /$SITE/index.php [L]
 </IfModule>" > $DIR$SITE/.htaccess
 
-# zamiana bazy danych w configu
-sed -i -e "s/m1547_wp_starterpack/$MYSQL_SITE_DB/g" "$DIR$SITE/wp-config.php"
+echo "Preapring config"
 
+head -n 8 wp-config-sample.php | cat > "$DIR$SITE/wp-config.php"
+
+wget https://api.wordpress.org/secret-key/1.1/salt/
+cat "index.html" >> "$DIR$SITE/wp-config.php"
+rm "index.html"
+
+tail -n 5 wp-config-sample.php | cat >> "$DIR$SITE/wp-config.php"
+
+# zamiana bazy danych w configu
+sed -i -e "s/database_name_here/$MYSQL_SITE_DB/g" "$DIR$SITE/wp-config.php"
+sed -i -e "s/username_here/$ENV_MYSQL_USER/g" "$DIR$SITE/wp-config.php"
+sed -i -e "s/password_here/$ENV_MYSQL_PASS/g" "$DIR$SITE/wp-config.php"
+sed -i -e "s/localhost/$ENV_MYSQL_HOST/g" "$DIR$SITE/wp-config.php"
 
 echo "Site is ready: $DNS"
 echo "ALL DONE :)"
-
-
